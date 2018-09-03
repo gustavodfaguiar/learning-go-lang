0d38760678ff05187a5f008e31e31ac3d1ec9fed
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2018-09-02 22:01:19 -0300

    Learn scope

---
+++ src/scope/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var y int = 20
+
+func main() {
+	x := 10
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
---
+++ src/scope/other.go
@@ -0,0 +1,3 @@
+package main
+
+var z string = "teste"
