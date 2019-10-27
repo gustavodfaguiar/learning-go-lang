7233ff1963c81e0f1dc356090c8ca57cda8cb2b4
Gustavo Aguiar <gustavodfaguiar@gmail.com>
2019-10-27 17:33:20 -0300

    Add example quicksort

---
+++ func/func.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func imprimirDados(nome string, idade int) {
+	fmt.Printf("%s tem %d anos. \n", nome, idade)
+}
+
+func soma(n, m int) int {
+	return n + m
+}
+
+func main() {
+	imprimirDados("Gustavo", 27)
+
+	s := soma(3, 5)
+	fmt.Println("A soma é", s)
+}
